b75bddc8a7da419dcf0f181e2371466889f938a8
ducthinh2k3 <ducthinh12tn137@gmail.com>
2024-10-11 03:41:35 +0000

    update base

---
+++ lib/view/home/pages/home.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Expense Tracker'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.add),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Favorites',
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text('Home Page'),
+      ),
+    );
+  }
+}
